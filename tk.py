f0934b4ae069f420ec428379c71ea0a02cf13fa9
Hashioki <MarineJewelBlue@gmail.com>
2018-11-12 09:35:16 +0900

    variable"date" become String from Int

--- tk.py
+++ tk.py
@@ -15,20 +15,21 @@ def saveTask(bL,win):
     win.destroy()
 
 def addTskWin(event):
-    global aTW
-    if aTW is None or not aTW.winfo_exists(): 
-        aTW = tk.Toplevel()
-        aTW.title("タスクを追加")
+    global subW
+    if subW is None or not subW.winfo_exists(): 
+        subW = tk.Toplevel()
+        subW.title("タスクを追加")
         sw.saveTask = saveTask
-        aW = sw.InputWindow(aTW,dt.datetime.today())
+        aW = sw.InputWindow(subW,dt.datetime.today())
         aW.packWidget()
+        aW.tEntry.focus_set()
 
 def pushed(event):
     event.widget["text"] = "pushed"
 
 def btnLGen(tskL):
     if len(tskL) != 0:
-        btnText = str(tskL[0]["title"]) + "\n" + "date:" + str(tskL[0]["date"])
+        btnText = tskL[0]["title"] + "\n" + "date:" + tskL[0]["date"]
         btn = tk.Button(tskFrame,text=btnText)
         return [btn] + btnLGen(tskL[1:])
     else:
@@ -62,11 +63,11 @@ def renewTsk(tskL):
     
 taskList = []
 buTskL = []
-aTW = None
+subW = None
 
 #-------メインウィンドウ----------
 root = tk.Tk()
-root.title(u"WorkStack")
+root.title(u"Tasksack")
 root.geometry("300x400")
 
 #--------上部----------------
@@ -81,13 +82,15 @@ addBtn.bind("<1>",addTskWin)
 addBtn.pack(side="right")
 #----------------------------
 
+sortBtn.focus_set() #ウィンドウ生成時sortボタンにフォーカス
+
 #---------タスクリスト-----------
 tskFrame = tk.Frame(root,bd=2,relief="ridge")
 tskFrame.pack(fill="both")
 #-------テスト用-----------
-taskBox = {"title":"title","priority":0,"date":181224,"kind":"仕事","favorit":1,"comment":"comment"}
+taskBox = {"title":"title","priority":0,"date":"2018-11-3","kind":"仕事","favorit":1,"comment":"comment"}
 taskList.append(taskBox)
-taskBox = {"title":"ponpon","priority":2,"date":181103,"kind":"趣味","favorit":0,"comment":"cmcmcmc"}
+taskBox = {"title":"ponpon","priority":2,"date":"2018-2-22","kind":"趣味","favorit":0,"comment":"cmcmcmc"}
 taskList.append(taskBox)
 #------------------------
 
