3e737ddc18192651a72eea81cde7845ccde74d4b
Hashioki <MarineJewelBlue@gmail.com>
2018-11-30 13:52:33 +0900

    add sort func

--- tk.py
+++ tk.py
@@ -3,7 +3,9 @@ import tkinter as tk
 import datetime as dt
 import subWindow as sw
 
+#[button] -> window -> I/O
 def saveTask(bL,win):
+    #dict tsk
     tsk = {"title":bL["title"].get(),
            "priority":bL["priority"].get(),
            "date":bL["date"].get(),
@@ -14,41 +16,50 @@ def saveTask(bL,win):
     renewTsk(taskList)
     win.destroy()
 
+#event -> void
 def addTskWin(event):
     global subW
     if subW is None or not subW.winfo_exists(): 
         subW = tk.Toplevel()
         subW.title("タスクを追加")
-        sw.saveTask = saveTask
         aW = sw.InputWindow(subW,dt.datetime.today())
         aW.packWidget()
         aW.tEntry.focus_set()
 
+#event -> int -> void
 def pushed(event,i):
     global subW
     if subW is None or not subW.winfo_exists():
         subW = tk.Toplevel()
         subW.title("タスクを編集")
-        sw.saveTask = saveTask
         eW = sw.InputWindow(subW,dt.datetime.today(),btnTaskList[i])#ボタンに対応するタスクの情報を取りたい
         eW.packWidget()
         eW.tEntry.focus_set()
 
+#[task] -> [button]
+#ボタンの生成&初期設定を行う
 def btnLGen(tskL):
     if len(tskL) != 0:
         btnText = tskL[0]["title"] + "\n" + "date:" + tskL[0]["date"]
         btn = tk.Button(tskFrame,text=btnText)
+        #print(btnTaskList)
         btnTaskList.append(tskL[0])
         return [btn] + btnLGen(tskL[1:])
     else:
+        print("btnLGen END")
         return []
-
-def cmdSet(btnL,i):
+    
+#buttn -> int -> void
+def cmdSet(btnL,i=0):
     if len(btnL) != 0:
         #eWGen = lambda:pushed(i)
         #btnL[0].bind("<1>",eWGen)
         cmdSet(btnL[1:],i+1)
-
+    else:
+        pass
+    
+#[button] -> [task] -> void
+#ボタンを出現させる
 def btnGen(btnL,tskL):
     if len(btnL) != 0:
         btnL[0].pack(fill="both")
@@ -58,33 +69,29 @@ def btnGen(btnL,tskL):
             lbl = tk.Label(btnL[0],text="☆")
         lbl.pack(side="right")
         btnGen(btnL[1:],tskL[1:])
+    else:
+        pass
 
+        
+# frame -> I/O
 def frameClear(frm):
     widL = frm.pack_slaves()
-    print(widL)
+    #print(widL)
     for l in widL:
         l.destroy()
-    print(widL)
-
-def renewTsk(tskL):
-    frameClear(tskFrame)
-    btnTaskList = []
-    print(btnTaskList)
-    btnList = btnLGen(tskL)
-    cmdSet(btnList,0)
-    print(btnTaskList)
-    btnGen(btnList,tskL)  
     
+# void -> I/O
 def tskSort(self):
     if svSortV == menuBar.sortV:
         pass
-    elif menuBar.sortV == "優先度":
+    elif menuBar.sortV.get() == "優先度":
         btnTaskList.sort(key= lambda x:x["priority"])
         btnTaskList.reverse()
     else:
         btnTaskList.sort(key= lambda x:x["date"])
     renewTsk(btnTaskList)
 
+# void -> I/O
 def tskFilt(self):
     if menuBar.filtV.get() == "すべて":
         btnTaskList = taskList
@@ -93,11 +100,28 @@ def tskFilt(self):
     renewTsk(btnTaskList)
     pass
 
+# [task] -> I/O
+def renewTsk(tskL):
+    global btnTaskList
+    frameClear(tskFrame)
+    print("btnTaskList:",btnTaskList)
+    btnTaskList = []
+    #print("tskL:",tskL)
+    btnList = btnLGen(tskL)
+    #print("btnTaskList:",btnTaskList)
+    #print("btnList:",btnList)
+    cmdSet(btnList)
+    #print(btnTaskList)
+    btnGen(btnList,tskL)
+
+#---------------------------------------------
 taskList = []
-buTskL = []
+#buTskL = []
 subW = None
 svSortV = None
 btnTaskList = []
+sw.saveTask = saveTask
+
 
 #-------メインウィンドウ----------
 root = tk.Tk()
@@ -109,17 +133,18 @@ menuBar = sw.MnBar(root)
 menuBar.sortBtn.focus_set() #ウィンドウ生成時sortボタンにフォーカス
 #-------------------------------
 
-#--------上部----------------
+#--------上部メニュー&ボタン生成----------------
 spFrame = tk.Frame(root,bd=0,relief="ridge")
-spFrame.pack(fill="x")
 sortBtn = tk.Button(spFrame,text="to sort")
 sortBtn.bind("<1>",tskSort)
-sortBtn.pack(side="left")
 filtBtn = tk.Button(spFrame,text="to filter")
 filtBtn.bind("<1>",tskFilt)
-filtBtn.pack(side="left")
 addBtn = tk.Button(spFrame,text="+")
 addBtn.bind("<1>",addTskWin)
+
+spFrame.pack(fill="x")
+sortBtn.pack(side="left")
+filtBtn.pack(side="left")
 addBtn.pack(side="right")
 #----------------------------
 
